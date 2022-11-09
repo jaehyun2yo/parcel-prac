abbe289d49498b6d2ea9c83d686a56f16100d822
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-11-09 19:00:31 +0900

    create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM - 브라우저에서의 가져오기 내보내기 방식
+// CommenJS - NodeJS에서의 가져오기 내보내기 방식
+
+//import autoprefixer from 'autoprefixer'
+const autoprefixer = require("autoprefixer")
+
+//  export {
+//  plugins: [
+//         autoprefixer
+//     ]
+//  }
+module.exports = {
+    plugins: [
+        autoprefixer
+    ]
+}
